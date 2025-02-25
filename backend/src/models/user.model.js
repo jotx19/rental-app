27c1495dd7a63abdac23b6a98457ef40480a1488
Prabjot singh <prabjot24x7@gmsil.com>
2025-02-24 23:58:26 -0500

    profile image schema

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    profilePic: {
+      type: String,
+      default: "",
+    },
     otp:{
       type: String,
     },
