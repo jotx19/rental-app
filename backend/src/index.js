0538f2bbc7f3fc942515fb2b1db42c17b4d8f8c5
Prabjot singh <prabjot24x7@gmsil.com>
2025-05-02 09:44:32 -0400

    chechv4

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(
     cors({
     origin: "https://othousing.vercel.app",
-    credentials: 'include',
+    credentials: true,
 })
 );
 // app.use("/", (req, res) => {
