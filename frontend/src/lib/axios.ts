29839e2a34eff21c570a0fa045d044d9863b8e0e
Prabjot singh <prabjot24x7@gmsil.com>
2025-05-02 08:33:28 -0400

    checkv3

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://othousing.onrender.com": "/api",
+    baseURL:  "https://othousing.onrender.com/api",
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
