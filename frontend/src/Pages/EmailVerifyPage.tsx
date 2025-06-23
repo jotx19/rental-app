42b495e88265a45f48bfbc7dfcfe487a3fcc995b
Prabjot Singh <prabjot24x7@gmail.com>
2025-06-23 09:04:37 -0400

    auth-logic updatev2

--- frontend/src/Pages/EmailVerifyPage.tsx
+++ frontend/src/Pages/EmailVerifyPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Router, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import { useAuthStore } from '../store/useAuthStore';
 import { Loader, Mail } from 'lucide-react';
